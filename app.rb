9b6ed7d3319f4d1b5e361cb5cbf8e097817865d0
nico <nivanov4@jhu.edu>
2020-05-18 00:22:56 -0400

    added views folder with index.erb

--- app.rb
+++ app.rb
@@ -1,11 +1,8 @@
 require 'sinatra'
 
-cakes = Array.new
-file = File.open("cake.list", "r")
-file.each_line do |line|
-  cakes.push(line)
-end
 
-get '/' do
-  cakes
+
+get'/' do
+  @cakes = File.open("cake.list", "r")
+  erb :index
 end
