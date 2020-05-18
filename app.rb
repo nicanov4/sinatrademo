743ac01638a6db1a5978148cbb5e03acd5df47fa
nico <nivanov4@jhu.edu>
2020-05-18 17:58:20 -0400

    changed view file to only use jquery

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require 'sinatra'
-
-
+require 'sinatra/json'
 
 get'/' do
-  @cakes = File.open("cake.list", "r")
   erb :index
+end
+
+get'/cakes.json' do
+  @cakes = File.open("cake.list", "r")
+  @cakes_array = Array.new
+  @cakes.each_line do |line|
+    @cakes_array.push(line.split"\n")
+  end
+  json :cakes => @cakes_array
 end
