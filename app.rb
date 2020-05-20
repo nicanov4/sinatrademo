cc9ec1ea81de5902eff019dc60bb552407b3795c
nico <nivanov4@jhu.edu>
2020-05-20 19:21:11 -0400

    updating some files

--- app.rb
+++ app.rb
@@ -1,15 +1,31 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'pg'
 
 get'/' do
   erb :index
 end
 
+get'/cake/:id' do |id|
+  erb :cake, :locals => {:id => id}
+end
+                                                           
+get'/cake/:id/json' do |id|
+  con = PG.connect :dbname => 'cakedb', :user => 'nico',
+                   :password => 'password'
+  sql = "SELECT name FROM cakes WHERE id=#{id}"
+  result = con.exec(sql)
+  json result[0]
+end
+
 get'/cakes.json' do
-  @cakes = File.open("cake.list", "r")
-  @cakes_array = Array.new
-  @cakes.each_line do |line|
-    @cakes_array.push(line.split"\n")
+  con = PG.connect :dbname => 'cakedb', :user => 'nico',
+                   :password => 'password'
+  sql = "SELECT * FROM cakes"
+  result = con.exec(sql)
+  cakes = Array.new
+  result.each do |row|
+    cakes.push(row)
   end
-  json :cakes => @cakes_array
+  json :cakes => cakes
 end
