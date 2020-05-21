5c32b9082e435f31a5934fdfddb67bed9e802878
nico <nivanov4@jhu.edu>
2020-05-20 21:44:08 -0400

    fixing rake

--- db/migrate/20200520235132_create_articles.rb
+++ db/migrate/20200520235132_create_articles.rb
@@ -1,4 +1,12 @@
-class CreateArticles < ActiveRecord::Migration[5.2]
+class CreateArticles < ActiveRecord::Migration
   def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :content
+      t.boolean :published, :default => false
+      t.datetime :published_on, :required => false
+      t.integer :likes, :default => 0
+      t.timestamps null: false
+    end
   end
 end
