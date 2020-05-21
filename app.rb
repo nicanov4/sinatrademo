c17b1c413f7d16e84b920e6bceef6bbd713b6abc
nico <nivanov4@jhu.edu>
2020-05-21 13:20:27 -0400

    added ability to delete cakes and edit their names'

--- app.rb
+++ app.rb
@@ -14,6 +14,8 @@ get'/cake/:id' do |id|
 end
                                                            
 get'/cake/:id/json' do |id|
+  #con = PG.connect :dbname => 'cakedb', :user => 'nico',
+  #                 :password => 'password'
   con = PG.connect(ENV['DATABASE_URL'])
   sql = "SELECT name FROM cakes WHERE id=#{id}"
   result = con.exec(sql)
@@ -21,7 +23,9 @@ get'/cake/:id/json' do |id|
 end
 
 get'/cakes.json' do
-  con = PG.connect(ENV['DATABASE_URL'])
+  #con = PG.connect :dbname => 'cakedb', :user => 'nico',
+  #                 :password => 'password'
+  #con = PG.connect(ENV['DATABASE_URL'])
   sql = "SELECT * FROM cakes"
   result = con.exec(sql)
   cakes = Array.new
@@ -29,4 +33,25 @@ get'/cakes.json' do
     cakes.push(row)
   end
   json :cakes => cakes
+end
+
+post'/cake/:id/delete' do
+  id = params[:id]
+  #con = PG.connect :dbname => 'cakedb', :user => 'nico',
+  #                 :password => 'password'
+  con = PG.connect(ENV['DATABASE_URL'])
+  sql = "DELETE FROM cakes WHERE id=#{id}"
+  con.exec(sql)
+  redirect'.'
+end
+
+post'/cake/:id/edit' do
+  id = params[:id]
+  new_cake_name = params[:name]
+  #con = PG.connect :dbname => 'cakedb', :user => 'nico',
+  #                 :password => 'password'
+  con = PG.connect(ENV['DATABASE_URL'])
+  sql = "UPDATE cakes SET name = '#{new_cake_name}' WHERE id  = #{id}"
+  con.exec(sql)
+  redirect back
 end
