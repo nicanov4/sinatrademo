49a4c0964bb61d6303fed24010bf54495b67bcf2
nico <nivanov4@jhu.edu>
2020-05-21 00:19:30 -0400

    fixing pg

--- app.rb
+++ app.rb
@@ -14,16 +14,14 @@ get'/cake/:id' do |id|
 end
                                                            
 get'/cake/:id/json' do |id|
-  con = PG.connect :dbname => 'cakedb', :user => 'nico',
-                   :password => 'password'
+  con = PG.connect(ENV['DATABASE_URL'])
   sql = "SELECT name FROM cakes WHERE id=#{id}"
   result = con.exec(sql)
   json result[0]
 end
 
 get'/cakes.json' do
-  con = PG.connect :dbname => 'cakedb', :user => 'nico',
-                   :password => 'password'
+  con = PG.connect(ENV['DATABASE_URL'])
   sql = "SELECT * FROM cakes"
   result = con.exec(sql)
   cakes = Array.new
